ca321435cf3bc76b02459e21515424285cf59662
James McDonald <jamesmcdonald112@yahoo.ie>
2025-02-23 15:00:09 +0000

    Menu is dynamically rendered

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 import menuArray from './data.js'
+
+
+function getMenuHtml() {
+    return menuArray.map(({ name, ingredients, price, emoji, id}) => {
+        return `
+        <div class="menu-item">
+            <span class="food-emoji">${emoji}</span>
+            <div class="food-details">
+                <h2>${name}</h2>
+                <p class="food-ingredients">${ingredients.join(", ")}</p>
+                <p class="food-price">€${price}</p>
+            </div>
+            <button class="add-button" data-id=${id}>
+                <i class="fa-solid fa-plus"></i>
+            </button>
+        </div>
+        `
+        
+    }).join('')
+}
+
+document.getElementById('menu-container').innerHTML = getMenuHtml()
